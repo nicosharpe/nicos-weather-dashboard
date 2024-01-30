fe94bb3834d6747134f278bac02f0a28b05a60f4
nicosharpe <nicolesharpe11@gmail.com>
2024-01-30 22:51:43 +0000

    added README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+https://nicosharpe.github.io/nicos-weather-dashboard/
+
 # nicos-weather-dashboard
+
 A 5 Day Weather Forecast to retrieve weather data for cities.
+
+
+# Installation
+
+N/A
+
+
+# Usage
+
+Simply type in a city name and hit search, you will then be presented with the city, date, an icon representation of weather conditions,  temperature, humidity and wind speed.
+
+*TO COME - You will also be shown a 5 day weather forecast for the city you searched for*
+
+Your searches will be save in 'Recent Cities' as a button, you can simply click on the buttons of your previous searches to quickly see weather results for the city of your choice.
+
+
+# Lisence
+
+See LISENCE section in repository
+
+# Credit
+
+Openweather API
--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 $(document).ready(function () {
-var weather = $("<h3>", {id: "weather"})
-var temp = $("<h4>", {id: "temperature"});
-var wind = $("<h4>", {id: "wind"});
-var humidity = $("<h4>", {id: "humidity"});
-$("#forecast").append(weather,temp,wind,humidity);
+  var weather = $("<h3>", { id: "weather" });
+  var temp = $("<h4>", { id: "temperature" });
+  var wind = $("<h4>", { id: "wind" });
+  var humidity = $("<h4>", { id: "humidity" });
+  $("#forecast").append(weather, temp, wind, humidity);
+
+  // Load recently searched cities from localStorage
+  var recentCities = JSON.parse(localStorage.getItem("recentCities")) || [];
+
+  // Function to render buttons for each searched term
+  function renderButtons() {
+    // Deleting the recent city buttons prior to adding new buttons
+    $("#recent-cities").empty();
+
+    // Looping through the array of recent cities
+    for (var i = 0; i < recentCities.length; i++) {
+      // Check if the button already exists
+      if ($("#recent-cities").find("[data-name='" + recentCities[i] + "']").length === 0) {
+        // Dynamically generating buttons for each recent city
+        var button = $("<button>");
+        button.addClass("recent-city btn btn-secondary justify-content-between");
+        button.attr("data-name", recentCities[i]);
+        button.text(recentCities[i]);
+        $("#recent-cities").append(button);
+      }
+    }
+  }
 
   $("#city-search").on("click", function (event) {
     event.preventDefault();
     var searchTerm = $("#search-input").val().trim();
     var geoQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=1b80d8a7edd459f41fc968c54929e7ff";
-    var todaysDate = dayjs().format("DD[/]MM[/]YY")
+    var todaysDate = dayjs().format("DD[/]MM[/]YY");
 
-
-
-    // Creates a Fetch call for the specific movie button being clicked
+    // Creates a Fetch call
     fetch(geoQueryURL)
       .then(function (response) {
         return response.json();
@@ -42,27 +62,58 @@ $("#forecast").append(weather,temp,wind,humidity);
       .then(function (data) {
         console.log(data);
         // var weatherIcon
-        $(temp).text("Temp: " + data.list[0].main.temp.toFixed(1) + "°C")
+        $(temp).text("Temp: " + data.list[0].main.temp.toFixed(1) + "°C");
         $(wind).text("Wind Speed: " + data.list[0].wind.speed);
-        $(humidity).text("Humidity: " + data.list[0].main.humidity +"%");
+        $(humidity).text("Humidity: " + data.list[0].main.humidity + "%");
 
-         // Display weather icon
-         var iconCode = data.list[0].weather[0].icon;
-         var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-         $("#weather-icon").attr("src", iconUrl).attr("alt", "Weather Icon");
- 
+        // Display weather icon
+        var iconCode = data.list[0].weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        $("#weather-icon").attr("src", iconUrl).attr("alt", "Weather Icon");
 
-        //create button for each searched term
-        var recentCityBtn = $("<button>").text(searchTerm).on("click", function () {
-          $("#search-input").val(searchTerm);
-          $("#city-search").click();
-        });
+        // Save the searched city to localStorage
+        recentCities.push(searchTerm);
+        localStorage.setItem("recentCities", JSON.stringify(recentCities));
 
-        
-      })
-      .catch(function (error) {
-        console.error('Error:', error);
+        // Render the buttons for previous cities
+        renderButtons();
+
+        // Container to append cards
+        const weatherCardsContainer = $("#weatherCards");
+        weatherCardsContainer.empty(); // Clear existing cards
+
+        // Get the current date
+        const currentDate = dayjs();
+
+        // Loop through next 5 days to create cards
+        for (let i = 0; i < 5; i++) {
+          // Calculate the date for each day
+          const dateForDay = currentDate.add(i, 'day').format('YYYY-MM-DD');
+
+          // Create card element
+          const card = $("<div>").addClass("col-md-4 mb-4");
+
+          // Create Bootstrap card structure
+          card.html(`
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">${dateForDay}</h5>
+                <p class="card-text">Weather forecast for ${dateForDay}</p>
+                <!-- You can customize the content of each card here based on your data -->
+              </div>
+            </div>
+          `);
+
+          // Append card to the container
+          weatherCardsContainer.append(card);
+        }
       });
   });
-  
+
+  // Event delegation for dynamically created recent city buttons
+  $("#recent-cities").on("click", ".recent-city", function () {
+    // On button click, set the search input and perform a new search
+    $("#search-input").val($(this).attr("data-name"));
+    $("#city-search").click();
+  });
 });
